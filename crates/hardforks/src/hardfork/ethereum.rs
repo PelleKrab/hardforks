ce9e5d736411e86624b2740c5c7d4082953869e0
pellekrab <78560773+PelleKrab@users.noreply.github.com>
2025-09-11 10:48:07 -0600

    removed eip7805 fork at_block fn

--- crates/hardforks/src/hardfork/ethereum.rs
+++ crates/hardforks/src/hardfork/ethereum.rs
@@ -713,12 +713,6 @@ pub trait EthereumHardforks {
         self.is_ethereum_fork_active_at_timestamp(EthereumHardfork::Eip7805, timestamp)
     }
 
-    /// Convenience method to check if [`EthereumHardfork::Eip7805`] is active at a given block
-    /// number.
-    fn is_eip7805_active_at_block(&self, block_number: u64) -> bool {
-        self.is_ethereum_fork_active_at_block(EthereumHardfork::Eip7805, block_number)
-    }
-
     /// Convenience method to check if [`EthereumHardfork::Bpo1`] is active at a given timestamp.
     fn is_bpo1_active_at_timestamp(&self, timestamp: u64) -> bool {
         self.is_ethereum_fork_active_at_timestamp(EthereumHardfork::Bpo1, timestamp)
